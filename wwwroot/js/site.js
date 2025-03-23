5841a00f3a182a3e15b6757ea0e7cbd8b6bbd8a8
soaalice <amboararampanana@gmail.com>
2025-03-23 13:03:18 +0300

    Add loading indicator for forms

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -2,3 +2,8 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function showLoadingState(buttonId, spinnerId) {
+    document.getElementById(spinnerId).style.display = "inline-block";
+    document.getElementById(buttonId).disabled = true;
+    document.getElementById(buttonId).innerHTML = '<span id="' + spinnerId + '" class="spinner"></span> Chargement...';
+}
